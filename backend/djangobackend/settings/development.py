10980f58e631550d3497cbe3efa9475867e5eea8
loganrobertclemons <loganrobertclemons@gmail.com>
2020-02-04 16:37:54 -0600

    Another Commit

--- backend/djangobackend/settings/development.py
+++ backend/djangobackend/settings/development.py
@@ -4,7 +4,7 @@ from djangobackend.settings.base import *
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '47(6^lv2h6lo*2cfr#v02021$8p(wvwgivq9+=3f3r5=hbsu%r'
+SECRET_KEY = '47(6^lv2h6lo*2cfr#v02021$8p(wvwgivq9+=3f3r5=hbsu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
